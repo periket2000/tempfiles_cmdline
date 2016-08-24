4439b86ec897d6f62d1ef230fcbb06dee6f605fb
Marco Albero <marco@mybi.es>
2016-08-24 12:34:42 +0200

    skeleton for python pypi package development

---
+++ README.md
@@ -0,0 +1,15 @@
+# [Tempfil.es] command line tool
+
+This tool is intended to provide command line access to the ephemeral/anonymous file sharing service [Tempfil.es]. 
+
+### Building the package locally
+```python
+python setup.py build
+```
+
+### Installing the package locally
+```python
+python setup.py install
+```
+
+[Tempfil.es]: <https://tempfil.es>
---
+++ src/tempfiles-cmdline/__init__.py
@@ -0,0 +1,18 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+__version__ = "1.0.0"
+__description__ = "Tempfil.es ephemeral/anonymous file sharing service command line tool"
+__license__ = "Apache License, Version 2.0"
+__uri__ = "https://tempfil.es"
+__author__ = "Marco Antonio Albero Albero"
+__email__ = "marco@mybi.es"
